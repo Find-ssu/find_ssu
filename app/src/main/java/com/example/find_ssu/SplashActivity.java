23ba6b098c3167d744842670e5dc1cc8d82ab7ef
sarahstar <star092312@gmail.com>
2023-11-16 19:23:12 +0900

    제발 오늘 마지막 머지 !!

--- app/src/main/java/com/example/find_ssu/SplashActivity.java
+++ app/src/main/java/com/example/find_ssu/SplashActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.find_ssu.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
