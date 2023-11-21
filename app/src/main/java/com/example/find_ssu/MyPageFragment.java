d05c8b0c7a8316db9806339ec4ffcd66b53e2528
sarahstar <star092312@gmail.com>
2023-11-21 18:53:12 +0900

    logout & link firebase_storage,firestore

--- app/src/main/java/com/example/find_ssu/MyPageFragment.java
+++ app/src/main/java/com/example/find_ssu/MyPageFragment.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MyPageFragment extends Fragment {
+    private static final String TAG = "FINDSSU";
 
     private TextView my_page_logout_tv;
 
@@ -30,13 +34,26 @@ public class MyPageFragment extends Fragment {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //파이어베이스 로그아웃
+                signOut();
+                getActivity().finish();
                 // 로그아웃 버튼을 클릭하면 LoginActivity로 이동
-                Intent intent = new Intent(requireContext(), LoginActivity.class);
-                startActivity(intent);
-                requireActivity().getSupportFragmentManager().beginTransaction().remove(MyPageFragment.this).commit();
+                //Intent intent = new Intent(requireContext(), LoginActivity.class);
+                //startActivity(intent);
+                //requireActivity().getSupportFragmentManager().beginTransaction().remove(MyPageFragment.this).commit();
             }
         });
 
         return view;
+    }
+    private void signOut() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+
+        // Check if there is no current user.
+        if (firebaseAuth.getCurrentUser() == null)
+            Log.d(TAG, "signOut:success");
+        else
+            Log.d(TAG, "signOut:failure");
     }
 }
