7d75ef438f4b30a64f94afb842caad5149c61c2a
sarahstar <star092312@gmail.com>
2023-11-28 00:48:14 +0900

    FindFabClickActivity생성

--- app/src/main/java/com/example/find_ssu/FindFragment.java
+++ app/src/main/java/com/example/find_ssu/FindFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -23,7 +24,6 @@ import com.firebase.ui.firestore.paging.FirestorePagingOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.example.find_ssu.databinding.FragmentFindBinding;
-import com.example.find_ssu.databinding.FragmentFindFabClickBinding;
 import com.example.find_ssu.databinding.ItemviewBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -48,24 +48,16 @@ public class FindFragment extends Fragment {
         findBinding = FragmentFindBinding.inflate(inflater,container,false);
         View rootview = findBinding.getRoot();
 
-        findBinding.findRv.setLayoutManager(new LinearLayoutManager(requireContext()));
-       // findBinding.findRv.setAdapter(new MyAdapter(list));
-
         findBinding.findFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 새로운 Fragment 객체 생성
-                Fragment findFabClickFragment = new FindFabClickFragment();
-
-                // Fragment 전환
-                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_find, findFabClickFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                // 액티비티 전환
+                Intent intent = new Intent(requireContext(), FindFabClickActivity.class);
+                startActivity(intent);
                 findBinding.findFab.setVisibility(View.GONE);
             }
         });
+
 
         findBinding.findMapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +70,13 @@ public class FindFragment extends Fragment {
         });
 //        initializeCloudFirestore();
 //        getAllDocumentsInACollection();
+        return rootview;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         Query baseQuery = FirebaseFirestore.getInstance()
                 .collection("FindPost")
                 .orderBy("timestamp", Query.Direction.DESCENDING);
@@ -106,34 +105,32 @@ public class FindFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         recyclerView.setAdapter(adapter);
 
-
-        return rootview;
-
     }
-    private void getAllDocumentsInACollection() {
-        db.collection("FindPost")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-    }
-    private void initializeCloudFirestore() {
-        // Access a Cloud Firestore instance from your Activity
-        db = FirebaseFirestore.getInstance();
-    }
+    //    private void getAllDocumentsInACollection() {
+//        db.collection("FindPost")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//    private void initializeCloudFirestore() {
+//        // Access a Cloud Firestore instance from your Activity
+//        db = FirebaseFirestore.getInstance();
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
@@ -156,48 +153,4 @@ public class FindFragment extends Fragment {
         super.onStop();
         adapter.stopListening();
     }
-
-//    private class MyViewHolder extends RecyclerView.ViewHolder{
-//        private ItemviewBinding binding;
-//
-//        private MyViewHolder(ItemviewBinding binding){
-//            super(binding.getRoot());
-//            this.binding = binding;
-//        }
-//
-//        private void bind(String text){
-//            binding.itemNameTv.setText(text);
-//        }
-//    }
-//
-//    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
-//        private List<String> list;
-//
-//        private MyAdapter(List<String> list) {this.list = list;}
-//
-//        @Override
-//        @NonNull
-//        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-//            ItemviewBinding binding = ItemviewBinding.inflate(
-//                    LayoutInflater.from(parent.getContext()),
-//                    parent,
-//                    false
-//            );
-//            return new MyViewHolder(binding);
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-//            String text = list.get(position);
-//            holder.bind(text);
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return list.size();
-//        }
-//    }
-
-
-
 }
