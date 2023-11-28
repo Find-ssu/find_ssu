520b21a3e3a18f1de13730b9146fddcefc5d9585
kwakeunjae <emma_0407@naver.com>
2023-11-28 21:30:57 +0900

    뒤로가기 문제 해결

--- app/src/main/java/com/example/find_ssu/LookForFabClickActivity.java
+++ app/src/main/java/com/example/find_ssu/LookForFabClickActivity.java
@@ -145,6 +145,7 @@ public class LookForFabClickActivity extends AppCompatActivity {
                     public void onSuccess(Void aVoid) {
                         Intent intent = new Intent(LookForFabClickActivity.this,MainActivity.class);
                         startActivity(intent);
+                        finish();
                         Log.d(TAG, "업로드 후 프레그먼트 전환");
                         Toast.makeText(LookForFabClickActivity.this, "게시물 업로드 완료", Toast.LENGTH_SHORT).show();
                     }
