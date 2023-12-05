89d3e4cc5064bc24a3a8a57234dd44eed28278ef
kwakeunjae <emma_0407@naver.com>
2023-12-05 18:51:39 +0900

    쿼리 어댑터 -> 리스트 어댑터 : 검색기능 완료 앱 이름 : 찾아가ssu

--- app/src/main/java/com/example/find_ssu/Find/FindFragment.java
+++ app/src/main/java/com/example/find_ssu/Find/FindFragment.java
@@ -7,6 +7,8 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.paging.PagingConfig;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,8 +19,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.find_ssu.Map.MapsActivity;
+import com.example.find_ssu.R;
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
 import com.example.find_ssu.databinding.FragmentFindBinding;
@@ -26,25 +30,56 @@ import com.example.find_ssu.databinding.ItemviewBinding;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 
 public class FindFragment extends Fragment {
-    private FirestorePagingAdapter<FindPost,FindPostViewHolder> adapter;
+    private ArrayList<FindPost> list = new ArrayList<>();
+    private ArrayList<FindPost> originalList = new ArrayList<>(); // 원본 데이터 보존
+    private FindAdapter adapter;
     private static final String TAG = "FINDSSU";
     private FirebaseFirestore db;
-    FragmentFindBinding findBinding;
+    private FragmentFindBinding findBinding;
+
+    private RecyclerView find_rv_view;
+    private EditText find_search_word_et;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        adapter = new FindAdapter(requireContext(), list);
+
+        adapter.setOnItemClickListener(new FindAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(FindPost findPost) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.home_navi, FindClickFragment.newInstance(findPost));
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        findBinding = FragmentFindBinding.inflate(inflater, container, false);
+        View rootView = findBinding.getRoot();
 
+        find_rv_view = findBinding.findRv;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        find_rv_view.setLayoutManager(layoutManager);
+        find_rv_view.setAdapter(adapter);
 
-        findBinding = FragmentFindBinding.inflate(inflater,container,false);
-        View rootview = findBinding.getRoot();
-        //글쓰기버튼 클릭이벤트
+        find_search_word_et = findBinding.findSearchWordEt;
+
+        // 글쓰기 버튼 클릭 이벤트
         findBinding.findFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,53 +89,24 @@ public class FindFragment extends Fragment {
             }
         });
 
-        // EditText에서 텍스트가 변경될 때 호출되는 메서드
-        findBinding.findSearchWordEt.addTextChangedListener(new TextWatcher() {
+        find_search_word_et.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
 
-            Query originalQuery = FirebaseFirestore.getInstance()
-                    .collection("FindPost")
-                    .orderBy("timestamp", Query.Direction.DESCENDING);
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // 검색어가 변경될 때마다 호출되는 부분
                 String searchText = charSequence.toString().trim();
-
-                if (searchText.isEmpty()) {
-                    PagingConfig config = new PagingConfig(4, 2, false);
-                    FirestorePagingOptions<FindPost> newOptions = new FirestorePagingOptions.Builder<FindPost>()
-                            .setLifecycleOwner(getViewLifecycleOwner())
-                            .setQuery(originalQuery, config, FindPost.class)
-                            .build();
-
-                    adapter.updateOptions(newOptions); // 어댑터에 새로운 옵션을 업데이트
-
-                } else {
-                    Query filteredQuery = FirebaseFirestore.getInstance()
-                            .collection("FindPost")
-                            .orderBy("timestamp", Query.Direction.DESCENDING)
-                            .whereEqualTo("name", searchText); // 여기서 "name"은 검색하려는 필드명입니다.
-
-                    PagingConfig config = new PagingConfig(4, 2, false);
-                    FirestorePagingOptions<FindPost> newOptions = new FirestorePagingOptions.Builder<FindPost>()
-                            .setLifecycleOwner(getViewLifecycleOwner())
-                            .setQuery(filteredQuery, config, FindPost.class)
-                            .build();
-
-                    adapter.updateOptions(newOptions); // 어댑터에 새로운 옵션을 업데이트
-                }
+                filterData(searchText);
             }
-
 
             @Override
             public void afterTextChanged(Editable editable) {
 
             }
         });
-
-
 
         //지도버튼 클릭이벤트
         findBinding.findMapBtn.setOnClickListener(new View.OnClickListener() {
@@ -110,77 +116,64 @@ public class FindFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        return rootview;
+
+        return rootView;
+    }
+
+    private void filterData(String searchText) {
+
+        if (originalList.isEmpty()) {
+            originalList.addAll(list); // 원본 데이터를 초기화
+        }
+
+        ArrayList<FindPost> filteredList = new ArrayList<>();
+
+        if (searchText.isEmpty()) {
+            filteredList.addAll(originalList); // 검색어가 비어있을 때 원본 데이터 보여주기
+        } else {
+            // 검색어로 필터링된 결과를 리스트에 추가
+            for (FindPost post : originalList) {
+                if (post.getName().toLowerCase().contains(searchText.toLowerCase())) {
+                    filteredList.add(post);
+                }
+            }
+        }
+
+        adapter.updateList(filteredList); // 필터링된 결과를 어댑터에 전달하여 업데이트
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //FindPost콜렉션 시간순으로 갖고옴
-        Query baseQuery = FirebaseFirestore.getInstance()
-                .collection("FindPost")
-                .orderBy("timestamp", Query.Direction.DESCENDING);
-        Log.d(TAG, baseQuery.toString());
 
-        //페이징
-        PagingConfig config = new PagingConfig( 4, 2,false);
+        loadDataFromFirestore();
 
-        FirestorePagingOptions<FindPost> options = new FirestorePagingOptions.Builder<FindPost>()
-                .setLifecycleOwner(getViewLifecycleOwner())
-                .setQuery(baseQuery, config, FindPost.class)
-                .build();
-
-        //FirestorePagingAdapter
-        adapter = new FirestorePagingAdapter<FindPost, FindPostViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull FindPostViewHolder holder, int position, @NonNull FindPost model) {
-                holder.bind(model);
-                holder.position=position;
-                holder.model=model;
-            }
-            @NonNull
-            @Override
-            public FindPostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                Log.d(TAG, "onCreateViewHolder()");
-
-                ItemviewBinding binding=ItemviewBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
-                return new FindPostViewHolder(binding);
-            }
-        };
-        //리사이클러뷰에 어뎁터 연결
-        RecyclerView recyclerView = findBinding.findRv;
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        recyclerView.setAdapter(adapter);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                if (getFragmentManager() != null) {
-                    getFragmentManager().popBackStack(); // 이전 프래그먼트로 돌아가기
-                    findBinding.findFab.setVisibility(View.VISIBLE);
-                }
-            }
-        });
+    private void loadDataFromFirestore() {
+        db.collection("FindPost")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        list.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String name = document.getString("name");
+                            String location = document.getString("location");
+                            String location_detail = document.getString("location_detail");
+                            String date = document.getString("date");
+                            String more = document.getString("more");
+                            String img = document.getString("image");
+                            String documentuid = document.getString("documentuid");
+                            String uid = document.getString("uid");
+
+                            FindPost findPost = new FindPost(name, location, location_detail, date, more, img, documentuid, uid);
+                            adapter.addItem(findPost);
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
     }
-    @Override
-    public void onStart() {
-        Log.d(TAG, "onStart");
-
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
-    }
-
-
-
-
 }
