7484d5835b08a2d01601e8930578e356c278f6c5
sunnny619 <ellieella2031@gmail.com>
2023-12-07 12:47:29 +0900

    쪽지 알림 구현 시도

---
+++ app/src/main/java/com/example/find_ssu/Chat/FirebaseMessageService.java
@@ -0,0 +1,67 @@
+package com.example.find_ssu.Chat;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.find_ssu.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class FirebaseMessageService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        //token을 서버로 전송
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String CHANNEL_ID = "one-channel";
+        String CHANNEL_NAME = "My Channel One1";
+        //수신한 메시지를 처리
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+            builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        } else {
+            builder = new NotificationCompat.Builder(getApplicationContext());
+        }
+
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        builder.setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.findssu);
+
+        Notification notification = builder.build();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(0, notification);
+    }
+}
